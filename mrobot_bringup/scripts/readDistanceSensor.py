72becdc692937ee5cf41e2feab910868673866c3
381643188@qq.com <381643188@qq.com>
2017-04-21 17:53:52 +0800

    update

--- mrobot_bringup/scripts/readDistanceSensor.py
+++ mrobot_bringup/scripts/readDistanceSensor.py
@@ -7,7 +7,7 @@ import time
 from mrobot_bringup.msg import sr04
 SERIAL_TIMEOUT = 2
 TTY_NAME = "/dev/ttyACM0"
-TTY_BAUDRATE = 57600
+TTY_BAUDRATE = 115200
 
 class ReadDistanceSensor(object):
     def __init__(self):
@@ -22,17 +22,20 @@ if __name__ == "__main__":
     print ('This is readDistanceSensor')
     read = ReadDistanceSensor()
     rospy.init_node('sr04_publisher', anonymous=True)
-    pub = rospy.Publisher('sr04_distance', sr04, queue_size=3)
+    pub = rospy.Publisher('sr04_distance', sr04, queue_size=10)
 
     while True:
         line = read.read()
         datas = line.split(",")
         if len(datas) == 4:
             sr = sr04()
-            sr.front = int(datas[0])
-            sr.left = int(datas[1])
-            sr.right = int(datas[2])
-            sr.back = int(datas[3])
-            pub.publish(sr)
-        print line
-        time.sleep(1)
+            try:
+                sr.front = int(datas[0])
+                sr.left = int(datas[1])
+                sr.right = int(datas[2])
+                sr.back = int(datas[3])
+                pub.publish(sr)
+            except Exception, e:
+                print e
+        print "string = %s, time = %f" % (line, time.time())
+        time.sleep(0.2)
